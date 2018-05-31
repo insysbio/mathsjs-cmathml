bf41c03a9e1bb53611d537d88fa43231342f8b69
Victoria Tkachenko <tkachenko@insysbio.ru>
2018-05-31 12:02:15 +0300

    add x*(x*y) and x*(x+y) demo cases

--- demo/cases.js
+++ demo/cases.js
@@ -9,7 +9,9 @@ const cases = [
   't==1 or t==2 or t ==3',
   '[1, 2, 3]',
   'a=1; b=2; c=3',
-  'x*y/z+a*exp(t)*log(15)'
+  'x*y/z+a*exp(t)*log(15)',
+  'x*(x*y)',
+  'x*(x+y)',
 ];
 
 const cases2 = [
@@ -18,7 +20,7 @@ const cases2 = [
   'k3*gal+k1*lac/K_l+k2*alac/K_a+k44*x1+k44*x2',
   'V___1___+V___2___-V___3___-V___4___',
   '1+K2_H1_D/10^(-6.3)+10^(-6.3)/K2_H2_D',
-	'alf_pH_D*(1+NAD/Kd_NAD_D+NADH/Kd_NADH_D)+bet_pH_D*(Hol/Kd_Hol_D+Hol*NAD/Kd_NAD_Hol_D/Kd_NAD_D)+gam_pH_D*(NADH*Hal/Kd_Hal_NADH_D/Kd_Hal_D+Hal/Kd_Hal_D+NAD*Hal/Kd_Hal_NAD_D/Kd_Hal_D+His*NADH/Kd_NADH_His_D/Kd_NADH_D+His/Kd_His_D)',
+  'alf_pH_D*(1+NAD/Kd_NAD_D+NADH/Kd_NADH_D)+bet_pH_D*(Hol/Kd_Hol_D+Hol*NAD/Kd_NAD_Hol_D/Kd_NAD_D)+gam_pH_D*(NADH*Hal/Kd_Hal_NADH_D/Kd_Hal_D+Hal/Kd_Hal_D+NAD*Hal/Kd_Hal_NAD_D/Kd_Hal_D+His*NADH/Kd_NADH_His_D/Kd_NADH_D+His/Kd_His_D)',
   '(1+L_dm*F_dm^(n-1)*H_dm^n)/(1+L_dm*(F_dm*H_dm)^n)',
   'V___5___*2.000-V___7___',
   'SDH*((kf_SDH/(1+Sal/Ki_Sal_SDH))*Suc_SDH*Q_SDH-kr_SDH*Fum_SDH*QH2_SDH)/(1+Suc_SDH+Q_SDH*Km_i_Suc_SDH/Kd_es_Suc_SDH+Suc_SDH*Q_SDH+Fum_SDH+Km_Fum_SDH*QH2_SDH/Kd_ef_Fum_SDH+Fum_SDH*QH2_SDH)',
@@ -51,7 +53,7 @@ const cases2 = [
   'x*5.1',
   'x*5.1e-3',
   'x*5e-3',
-	'x < 2 ? 1: 4',
-	'(x < 2 ? 1: 4)',
+  'x < 2 ? 1: 4',
+  '(x < 2 ? 1: 4)',
   '4+(a-3)'
 ];
